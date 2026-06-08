56600ff3da10879adb254497600a28d716d834b5
Rob Suderman <rob.suderman@gmail.com>
2026-06-07 21:29:45 -0700

    Lock to working HRX build

--- scripts/hrx/versions.sh
+++ scripts/hrx/versions.sh
@@ -1,6 +1,6 @@
 HRX_VERSION_REPOSITORY="https://github.com/ROCm/hrx.git"
 HRX_VERSION_FETCH_REF="refs/heads/main"
-HRX_VERSION_REF="36bb5cebc71da4367f30c1e88a5575081cd59816"
+HRX_VERSION_REF="f64a9cf2faa5b21d12d95caea21668962e0dabbc"
 
 LLAMA_VERSION_REPOSITORY="https://github.com/ROCm/llama.cpp.git"
 LLAMA_VERSION_FETCH_REF="refs/heads/hrx-integration"
